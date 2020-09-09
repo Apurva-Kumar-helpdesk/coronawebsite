330b12c56eb39019f898423ec44cae867bdab87d
Apurva Kumar <apurvakumar494@gmail.com>
2020-09-09 13:38:36 +0530

    Updates

---
+++ precache-manifest.33c30dfc3e5b44d07b54060a1b15efa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e644afaa255266cba59d7a6d55029043",
+    "url": "/coronawebsite/index.html"
+  },
+  {
+    "revision": "e87e5ab364e00e5b29f8",
+    "url": "/coronawebsite/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "d4970ee61f19e1ca8869",
+    "url": "/coronawebsite/static/css/main.94634e1c.chunk.css"
+  },
+  {
+    "revision": "e87e5ab364e00e5b29f8",
+    "url": "/coronawebsite/static/js/2.cf8dbd6d.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/coronawebsite/static/js/2.cf8dbd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4970ee61f19e1ca8869",
+    "url": "/coronawebsite/static/js/main.184396de.chunk.js"
+  },
+  {
+    "revision": "43955946e1a188661cc5",
+    "url": "/coronawebsite/static/js/runtime-main.e55f9f92.js"
+  }
+]);
